5c2b0c1a9025e110eedd7d8e32e6e85095e994fa
Rita Raher <ritaraher@gmail.com>
2019-05-17 18:19:35 +0100

    Delete new.py

--- new.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-
-
-connection = MongoClient('localhost', 27017)
-    #print(connection.database_names())
-database = connection.mongo #accessing the db, similar to use <db>
-collection = database.docs
-
